5c804a2656aed0f1a1c45e84d3b20edeab399c5b
Max Bernstein <tekknolagi@gmail.com>
2014-06-10 14:33:16 -0700

    form instead of manual name input

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,25 @@
-routie({
-    'days/:name': function (name) {
-	new DayListView({ name: name });
-    }
+$(document).ready(function () {
+    routie({
+	'': function () {
+	    $('#container').html('');
+	},
+	'days/:name': function (name) {
+	    new DayListView({ name: name });
+	}
+    });
+
+    $('#dayname').focus();
+
+    $('#dayclear').on('click', function (e) {
+	routie('');
+	$('#dayname').val('').focus();
+    });
+
+    $('#dayform').on('submit', function (e) {
+	e.preventDefault();
+	var name = $('#dayname');
+	routie('days/'+name.val());
+	name.val('');
+    });
+
 });
