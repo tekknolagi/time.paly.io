d814c55f164bcf7ab3e19c68b0f1387ac10a7324
Max Bernstein <tekknolagi@gmail.com>
2014-06-10 16:22:06 -0700

    pout != pin

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ class TimeApp < Grape::API
         {
           :date => d.pin.strftime('%b %d'),
           :pin => d.pin.strftime('%l:%M%P'),
-          :pout => d.pin.strftime('%l:%M%P')
+          :pout => d.pout.strftime('%l:%M%P')
         }
       }
 
